83b193494ec21083ae26d7ec5026d8c499de0999
Shane Lillie <ignignokterr@gmail.com>
2014-09-05 09:12:48 -0700

    Adding the DbInit app

---
+++ DbInit/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+using EnergonSoftware.Core;
+using EnergonSoftware.Database;
+
+namespace EnergonSoftware.DbInit
+{
+    [Serializable]
+    sealed class Configuration
+    {
+#region Singleton
+        private static Configuration _instance = new Configuration();
+        public static Configuration Instance { get { return _instance; } }
+#endregion
+
+#region Database Properties
+        private DatabaseDriver _accountDatabaseDriver = DatabaseDriver.SQLite;
+        private string _accountDatabaseFilename = "accounts.db";
+        private int _accountDatabasePoolSize = 10;
+
+        public DatabaseDriver AccountDatabaseDriver { get { return _accountDatabaseDriver; } }
+        public string AccountDatabaseFilename { get { return Path.Combine(Common.DataDir, _accountDatabaseFilename); } }
+        public int AccountDatabasePoolSize { get { return _accountDatabasePoolSize; } }
+#endregion
+
+        private Configuration()
+        {
+        }
+    }
+}
