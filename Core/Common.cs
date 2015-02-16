bc69bc0a2dc6840cd0750181ba570d11116c4734
Shane Lillie <ignignokterr@gmail.com>
2015-02-15 20:01:31 -0800

    Filesystem
    Moved some filesystem directories around

--- Core/Common.cs
+++ Core/Common.cs
@@ -13,17 +13,32 @@ namespace EnergonSoftware.Core
         public const int AuthVersion = 2;
 #endregion
 
+        public const string AppDir = "EnergonSoftware";
+
         // TODO: read this from the registry/CLI/whatever
-        public static string HomeDir { get { return Path.Combine("/", "EnergonSoftware"); } }
-        public static string BinDir { get { return Path.Combine(HomeDir, "bin"); } }
-        public static string ConfDir { get { return Path.Combine(HomeDir, "etc"); } }
-        public static string DataDir { get { return Path.Combine(HomeDir, "share"); } }
-        public static string LogDir { get { return Path.Combine(HomeDir, "var"); } }
+        private static string _installDir = Path.Combine("/", AppDir);
+        public static string InstallDir { get { return _installDir; } set { _installDir = value; } }
+
+        public static string BinDir { get { return Path.Combine(InstallDir, "bin"); } }
+        public static string EtcDir { get { return Path.Combine(InstallDir, "etc"); } }
+        public static string UsrDir { get { return Path.Combine(InstallDir, "usr"); } }
+        public static string VarDir { get { return Path.Combine(InstallDir, "var"); } }
+
+        public static string ConfDir { get { return Path.Combine(EtcDir, AppDir); } }
+        public static string DataDir { get { return Path.Combine(Path.Combine(UsrDir, "share"), AppDir); } }
+        public static string LogDir { get { return Path.Combine(Path.Combine(VarDir, "log"), AppDir); } }
 
         public static async Task InitFilesystemAsync()
         {
             Logger.Info("Initializing filesystem...");
+
+            await DirectoryExtensions.CreateDirectoryAsync(InstallDir).ConfigureAwait(false);
+
             await DirectoryExtensions.CreateDirectoryAsync(BinDir).ConfigureAwait(false);
+            await DirectoryExtensions.CreateDirectoryAsync(EtcDir).ConfigureAwait(false);
+            await DirectoryExtensions.CreateDirectoryAsync(UsrDir).ConfigureAwait(false);
+            await DirectoryExtensions.CreateDirectoryAsync(VarDir).ConfigureAwait(false);
+
             await DirectoryExtensions.CreateDirectoryAsync(ConfDir).ConfigureAwait(false);
             await DirectoryExtensions.CreateDirectoryAsync(DataDir).ConfigureAwait(false);
             await DirectoryExtensions.CreateDirectoryAsync(LogDir).ConfigureAwait(false);
