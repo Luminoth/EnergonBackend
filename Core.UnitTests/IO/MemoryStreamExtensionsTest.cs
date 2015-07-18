d17617b2d2e8dd2d48d3e73dda28c15efff52783
Shane Lillie <ignignokterr@gmail.com>
2015-07-17 21:56:09 -0700

    Removing moq
    Removing moq
    Adding a MemoryStreamExtensions test

---
+++ Core.UnitTests/IO/MemoryStreamExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnergonSoftware.Core.UnitTests.IO
+{
+    [TestClass]
+    public class MemoryStreamExtensionsTest
+    {
+        [TestMethod]
+        public void Compact_State()
+        {
+            // Arraynge
+            using(MemoryStream stream = new MemoryStream()) {
+
+                // Act
+                stream.WriteNetworkAsync("test").Wait();
+                stream.WriteNetworkAsync("one").Wait();
+                stream.WriteNetworkAsync("two").Wait();
+                stream.Flip();
+
+                string testString = stream.ReadNetworkStringAsync().Result;
+                stream.CompactAsync().Wait();
+
+                // Assert
+                Assert.AreEqual("test", testString);
+                Assert.AreEqual(stream.Length, stream.Position);
+                // TODO: test something more concrete here to "prove"
+                // that the buffer is actually compacted and not just flipped
+            }
+        }
+    }
+}
